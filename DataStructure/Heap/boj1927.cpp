f18956430813d6604878305ffc9ad97948843378
easy <0319easy@gmail.com>
2019-07-25 23:15:43 +0900

    solve: boj 1927

---
+++ DataStructure/Heap/boj1927.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	priority_queue<int, vector<int>, greater<int> > pq;
+
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; ++i) {
+		int x;
+		scanf("%d", &x);
+
+		if (x == 0) {
+			if (pq.empty())
+				printf("0\n");
+			else {
+				x = pq.top();
+				pq.pop();
+				printf("%d\n", x);
+			}
+		}
+		else {
+			pq.push(x);
+		}
+	}
+}
